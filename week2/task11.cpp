a6ce8105093d09d6ee2764146387c0ccf2a1f6fb
huzaifaarshad12 <144416664+huzaifaarshad12@users.noreply.github.com>
2023-10-03 12:00:17 +0500

    Add files via upload

---
+++ week2/task11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<" ------------------------------                                  "<<endl;
+cout<<"        O    ^___^                           "<<endl;
+cout<<"          O  (oo)\\_________                           "<<endl;
+cout<<"             (___)\\        )\\/\\                      "<<endl;
+cout<<"                  ||-----w  |                       "<<endl;
+cout<<"                  ||       ||                       "<<endl;
+}
---
+++ week2/task16.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main(){
+system("color 03");
+cout<<"            +--^---------- ------- ----- --------^-"                          <<endl;
+cout<<"                          '       '     '          '"                        <<endl;
+cout<<"             | |||||||||   '-------'     |           o"                     <<endl;
+cout<<"             '+--------------------------^-----------|"                     <<endl;
+cout<<"               ' --------- ---------- -------------'"                     <<endl;
+cout<<"                '         '          '                                               "<<endl;
+cout<<"                // XXXXXX //'|       //'                                                    "<<endl;
+cout<<"               // XXXXXX //  |      //'                                                    "<<endl;
+cout<<"              // XXXXXX //'-------'                                                     "<<endl;
+cout<<"             // XXXXXX //                                                    "<<endl;
+cout<<"            // XXXXXX //                                                    "<<endl;
+cout<<"           // XXXXXX //                                                    "<<endl;
+cout<<"          C__________C                                                    "<<endl;
+cout<<"           '________'                                                    "<<endl;
+
+}
